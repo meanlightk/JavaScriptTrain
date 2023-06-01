6605d178e9edabc659e2b6c4747199bd885d203b
DESKTOP-7IV49FD\503 <meanlightk@gmail.com>
2023-06-01 17:11:17 +0900

    조건문 및 반복문

---
+++ jtest02/test02.js
@@ -0,0 +1,32 @@
+let subject = "자바스크립트";
+switch (subject) {
+    case "C언어":
+        console.log("C++")
+        break;
+
+    case "자바스크립트":
+        console.log("JavaScript")
+        break;
+
+    case "자바":
+        console.log("Java")
+        break;
+        
+    default:
+        console.log("연습을 많이 하면 된다.")
+        break;
+}
+
+
+/*switch(표현식) {
+    case 값1:
+        명령문1
+        break;
+
+    case 값2:
+        명령문2
+        break;
+
+    dafault:
+        명령문3
+} */
---
+++ jtest02/test04.js
@@ -0,0 +1,13 @@
+let store = {snack: 1000, flower: 5000, beverage: 2000};
+
+for (let item in store) {
+    if (!store.hasOwnProperty(item)) continue;
+
+    console.log(item + "는 가격이" + store[item] + " 입니다.")
+}
+
+/*
+for (속성명 in 반복할 대상) {
+    
+}
+*/
