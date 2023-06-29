4f9c9ac2008d5ba831d65208a6ad3ee0d77afbe0
DESKTOP-7IV49FD\503 <meanlightk@gmail.com>
2023-06-29 18:00:28 +0900

    Node.js - Emitter

---
+++ jtest16/test09.js
@@ -0,0 +1,31 @@
+// 폴더 생성하기
+"use strict";
+
+const fs = require('fs');
+
+const checkDir = (path, callback) => {
+    fs.stat(path, (err, stats) => {
+        if (err && err.code === 'ENOENT') return callback(null, true);
+        if (err) return callback(err);
+
+        return callback(null, !stats.isDirectory());
+    });
+};
+
+const currentPath = __dirname;
+let path = `${currentPath}/js`;
+
+checkDir(paht, (err, isTrue) => {
+    if (err) return console.log(err);
+
+    if (!isTrue) {
+        console.log('이미 동일한 디렉터리가 있습니다. 디렉터리명을 변경합니다.');
+        path = `${currentPath}/js-new`;
+    }
+
+    fs.mkdir(path, (err) => {
+        if (err) console.log(err);
+
+        console.log(`${path} 경로로 디렉터리를 생성했습니다.`);
+    });
+});
